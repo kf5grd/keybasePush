5cc1fec523d7eb8d6548e34b90b4c11a0f5f71e9
Sam Hofius <dxb@keybase.io>
2018-12-26 22:33:20 -0500

    Remove requirement of -name at runtime. Use device name if -name not passed

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 const (
@@ -16,16 +15,14 @@ var instanceName string
 var serverPort int
 
 func init() {
-	flag.StringVar(&instanceName, "name", "", "Set the name of this instance (required)")
+	flag.StringVar(&instanceName, "name", "", "Set the name of this instance")
 	flag.IntVar(&serverPort, "port", 8617, "Set the port for the API")
 	flag.Parse()
 }
 
 func main() {
 	if instanceName == "" {
-		fmt.Println("Error: Instance name is required.\n")
-		flag.PrintDefaults()
-		os.Exit(1)
+		instanceName = KeybaseDeviceName()
 	}
 	router := NewRouter()
 
