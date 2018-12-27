254aa88f4c13da78ea015411c3735438dd32bd71
Sam Hofius <dxb@keybase.io>
2018-12-26 22:31:14 -0500

    Add keybase_status.go to get info from keybase status command

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 keybasePush : main.go repo.go web_handlers.go \
               web_logger.go web_message.go \
-              web_router.go web_routes.go
+              web_router.go web_routes.go \
+              keybase_status.go
 	go build -o keybasePush
 clean :
 	rm keybasePush
---
+++ keybase_status.go
@@ -0,0 +1,52 @@
+package  main
+
+import (
+	"encoding/json"
+	"os/exec"
+)
+
+var KeybaseStatus keybaseStatus
+
+func init() {
+	var err error
+	KeybaseStatus, err = GetKeybaseStatus()
+	if err != nil {
+		panic(err)
+	}
+}
+
+type keybaseStatus struct {
+	Username string `json:"Username"`
+	LoggedIn bool   `json:"LoggedIn"`
+	Device keybaseDevice `json:"Device"`
+}
+
+type keybaseDevice struct {
+	Name string `json:"name"`
+}
+
+func GetKeybaseStatus() (keybaseStatus, error) {
+	cmd := exec.Command("keybase", "status", "-j")
+
+	cmdOut, err := cmd.Output()
+	if err != nil {
+		return keybaseStatus{}, err
+	}
+
+	var retVal keybaseStatus
+	json.Unmarshal(cmdOut, &retVal)
+
+	return retVal, nil
+}
+
+func KeybaseDeviceName() string {
+	return KeybaseStatus.Device.Name
+}
+
+func KeybaseUsername() string {
+	return KeybaseStatus.Username
+}
+
+func KeybaseLoggedIn() bool {
+	return KeybaseStatus.LoggedIn
+}
