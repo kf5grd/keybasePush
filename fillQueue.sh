46dc7f20eec947807caecbc6df070e3d45ed9749
Sam Hofius <dxb@keybase.io>
2018-12-28 22:20:56 -0500

    Pass target names with uppercase letters. API should convert to lowercase

--- fillQueue.sh
+++ fillQueue.sh
@@ -2,6 +2,6 @@
 
 for i in {1..10}; do
     curl -H "Content-Type: application/json" \
-	 -d "{\"title\":\"message$i\",\"content\":\"asdf\",\"targets\":[\"server\"]}" \
+	 -d "{\"title\":\"message$i\",\"content\":\"asdf\",\"targets\":[\"Server\"]}" \
 	 http://localhost:8617/messages
 done
